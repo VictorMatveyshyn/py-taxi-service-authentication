c920a47669bb6d1d72f66f247c8ca446901f0ced
Victor Matveyshin <vic_m@ukr.net>
2026-09-15 22:54:37 +0300

    point 1

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -22,4 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("taxi.urls", namespace="taxi")),
+    path("taxi/", include("django.contrib.auth.urls")),
+    # path("__debug__/", include("debug_toolbar.urls", namespace="debug_toolbar") ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
